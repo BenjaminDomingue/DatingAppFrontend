ea8205c43b9d19edd6eef4e719ba1b71559af900
Benjamin Domingue <benjamin.domingue1991@gmail.com>
2020-03-08 23:09:19 -0400

    front end initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+/*decorator used for configuration.angular */
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+}
